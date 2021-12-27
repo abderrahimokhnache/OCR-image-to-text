51381af322ac5c7c8d0a8dfc91f46520ceb9fc9d
abderrahim mokhnache <93830072+astroxiii@users.noreply.github.com>
2021-12-27 10:17:48 +0100

    Update ITT.py

--- ITT.py
+++ ITT.py
@@ -5,30 +5,32 @@ from tkinter import filedialog
 
 
 root = Tk()
+root['bg'] = "#121212"
 root.title("ITT/S- Image to Text")
 
 def extraxt(e = None , lang = 'eng'):
 
-	result_as_a_text.delete(0.0,END)
-	txt = image_to_string(img , lang = lang)
-	result_as_a_text.insert(INSERT , txt)
+    result_as_a_text.delete(0.0,END)
+    txt = image_to_string(img , lang = lang)
+    result_as_a_text.insert(INSERT , txt)
 
 def set_image(e = None):
 
-	global img
-	img = Image.open(filedialog.askopenfilename())
-	try:
-		show_case['image'] = img
-		show_case.image = img
-		show_case['text'] = ""
-	except:
-		pass
-	extraxt()
+    global img
+    img = Image.open(filedialog.askopenfilename())
+    try:
+        show_case['image'] = img
+        show_case.image = img
+        show_case['text'] = ""
+    except:
+        pass
+    extraxt()
 
-show_case = Button(root , command = set_image , text = 'Open an Image ...')
+show_case = Button(root , command = set_image , text = 'Open an Image ...' 
+    ,bg ="#333", relief ='flat',fg ="#fff" )
 show_case.pack()
 
-result_as_a_text = Text(root)
+result_as_a_text = Text(root,bg ="#333", relief ='flat' ,fg = "#fff" ,font = ['Vernada' , 14])
 result_as_a_text.pack()
 
 root.mainloop()
